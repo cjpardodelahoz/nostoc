74d1a76214541778c589739b7b7a702ab5c76aff
cjpardodelahoz <cjpardodelahoz@gmail.com>
2023-08-20 16:05:27 -0400

    added region column to compilation and cleaned it

--- scripts/compile_public_rbclx_metadata.R
+++ scripts/compile_public_rbclx_metadata.R
@@ -1,7 +1,33 @@
 #!/usr/bin/env Rscript
 
-# Load required packages and functions
+##### Load required packages and functions ####
 library(tidyverse)
+# Function to extract part before the first semicolon
+extract_before_semicolon <- function(string) {
+  parts <- unlist(strsplit(string, ";")) # Split the string
+  trimmed_part <- trimws(parts[1])       # Get the first part and trim whitespace
+  return(trimmed_part)
+}
+extract_before_semicolon_v <- Vectorize(extract_before_semicolon, USE.NAMES = F)
+# Function to extract part before the first two commas
+extract_before_commas <- function(string) {
+  parts <- unlist(strsplit(string, ",")) # Split the string
+  trimmed_part_1 <- trimws(parts[1])       # Get the first part and trim whitespace
+  trimmed_part_2 <- trimws(parts[2])
+  result <- paste(trimmed_part_1, trimmed_part_2, sep = ", ")
+  return(result)
+}
+extract_before_commas_v <- Vectorize(extract_before_commas, USE.NAMES = F)
+# Function to extract part before the first comma
+extract_before_comma <- function(string) {
+  parts <- unlist(strsplit(string, ",")) # Split the string
+  trimmed_part <- trimws(parts[1])       # Get the first part and trim whitespace
+  return(trimmed_part)
+}
+extract_before_comma_v <- Vectorize(extract_before_comma, USE.NAMES = F)
+
+
+#### Curate and join metadata tables ####
 
 # Load voucher tables and extract DNA ID, DNA source, rbcLX accession, and
 # phylogroup
@@ -10,10 +36,13 @@ library(tidyverse)
 obrien_2013 <- read_csv(file = "misc_files/voucher_obrien2013.csv", 
                         locale=locale(encoding="latin1")) %>%
   rename(dna_id = collection_number) %>%
-  mutate(dna_source =paste(genus, species, sep = " ")) %>%
+  mutate(dna_source = paste(genus, species, sep = " ")) %>%
+  mutate(dna_source = str_replace(dna_source, "Peltigera", "P.")) %>%
+  mutate(dna_source = str_remove(dna_source, " Clade.*")) %>%
   rename(nostoc_phylogroup = rbclx_cluster) %>%
   add_column(reference = "obrien_et_al_2013") %>%
-  select(dna_id, dna_source, rbclx_accession, nostoc_phylogroup, reference)
+  add_column(region = "Canada, British Columbia") %>%
+  select(dna_id, dna_source, rbclx_accession, nostoc_phylogroup, region, reference)
 magain_2017 <- read_csv(file = "misc_files/voucher_magain2017.csv", 
                         locale=locale(encoding="latin1")) %>%
   rename(dna_source = Taxon) %>%
@@ -21,7 +50,11 @@ magain_2017 <- read_csv(file = "misc_files/voucher_magain2017.csv",
   rename(rbclx_accession = "rbcLX") %>%
   rename(nostoc_phylogroup = "rbcLX phylogroup or haplotype") %>%
   add_column(reference = "magain_et_al_2017") %>%
-  select(dna_id, dna_source, rbclx_accession, nostoc_phylogroup, reference) %>%
+  mutate(region = extract_before_semicolon_v(`Voucher/Published source`)) %>%
+  mutate(region = str_replace(region, "Qubec", "Quebec")) %>%
+  mutate(region = str_replace(region, "Iceland.*", "Iceland")) %>%
+  mutate(region = str_remove(region, ", Rudi et al. 1998")) %>%
+  select(dna_id, dna_source, rbclx_accession, nostoc_phylogroup, region, reference) %>%
   filter(rbclx_accession != "--") %>%
   mutate(dna_id = na_if(dna_id, "GB"))
 miadlikowska_2018 <- read_csv(file = "misc_files/voucher_miadlikowska2018.csv",
@@ -31,7 +64,14 @@ miadlikowska_2018 <- read_csv(file = "misc_files/voucher_miadlikowska2018.csv",
   rename(rbclx_accession = "rbcLX") %>%
   rename(nostoc_phylogroup = "Nostoc haplotype****") %>%
   add_column(reference = "miadlikowska_et_al_2018") %>%
-  select(dna_id, dna_source, rbclx_accession, nostoc_phylogroup, reference) %>%
+  mutate(region = extract_before_commas_v(Voucher)) %>%
+  mutate(region = str_replace(region, "Qubec", "Quebec")) %>%
+  mutate(region = str_remove(region, " J. Miadliko.*")) %>%
+  mutate(region = str_remove(region, ", N. Magain s.n.")) %>%
+  mutate(region = str_remove(region, ", H. Kristinsson L-27478")) %>%
+  mutate(region = str_remove(region, ", A. Agren 609")) %>%
+  mutate(region = str_replace(region, "U.S.", "USA")) %>%
+  select(dna_id, dna_source, rbclx_accession, nostoc_phylogroup, region, reference) %>%
   filter(rbclx_accession != "missing" & !is.na(rbclx_accession))
 chagnon_2018 <- read_csv(file = "misc_files/voucher_chagnon_2018.csv", 
                          locale=locale(encoding="latin1")) %>%
@@ -40,7 +80,8 @@ chagnon_2018 <- read_csv(file = "misc_files/voucher_chagnon_2018.csv",
   rename(rbclx_accession = "rbcLX accession number") %>%
   rename(nostoc_phylogroup = "rbcLX phylogroup") %>%
   add_column(reference = "chagnon_et_al_2018") %>%
-  select(dna_id, dna_source, rbclx_accession, nostoc_phylogroup, reference) %>%
+  mutate(region = extract_before_semicolon_v(`Voucher Information / Collection Site`)) %>%
+  select(dna_id, dna_source, rbclx_accession, nostoc_phylogroup, region, reference) %>%
   filter(rbclx_accession != "XXXXXXXX" & rbclx_accession != "YYYYYYYY")
 pardodelahoz_2018 <- read_csv(file = "misc_files/voucher_pardodelahoz2018.csv",
                               locale=locale(encoding="latin1")) %>%
@@ -49,7 +90,18 @@ pardodelahoz_2018 <- read_csv(file = "misc_files/voucher_pardodelahoz2018.csv",
   rename(rbclx_accession = "rbcLX (Nostoc)") %>%
   rename(nostoc_phylogroup = "Nostoc phylogroup or haplotype") %>%
   add_column(reference = "pardodelahoz_et_al_2018") %>%
-  select(dna_id, dna_source, rbclx_accession, nostoc_phylogroup, reference) %>%
+  mutate(region = extract_before_commas_v(Voucher)) %>%
+  mutate(region = str_replace(region, "U.S.", "USA")) %>%
+  mutate(region = str_replace(region, "columbia", "Columbia")) %>%
+  mutate(region = str_replace(region, "BC", "British Columbia")) %>%
+  mutate(region = str_replace(region, "Canada, Qu.*", "Canada, Quebec")) %>%
+  mutate(region = str_remove(region, ", B. Krzewicka 2512")) %>%
+  mutate(region = str_remove(region, ", I. Karlsson s.n.")) %>%
+  mutate(region = str_remove(region, ", N. Magain s.n.")) %>%
+  mutate(region = str_replace(region, "kray", "Kray")) %>%
+  mutate(region = str_replace(region, "Iceland, Langisj.*",  "Iceland, Langisjor")) %>%
+  mutate(region = str_replace(region, "ColombiaC", "Columbia")) %>%
+  select(dna_id, dna_source, rbclx_accession, nostoc_phylogroup, region, reference) %>%
   filter(rbclx_accession != "-")
 magain_2018 <- read_csv(file = "misc_files/voucher_magain2018.csv",
                         locale=locale(encoding="latin1")) %>%
@@ -57,7 +109,10 @@ magain_2018 <- read_csv(file = "misc_files/voucher_magain2018.csv",
   rename(rbclx_accession = "rbclx") %>%
   rename(nostoc_phylogroup = "rbclx_phylogroup_haplotype") %>%
   add_column(reference = "magain_et_al_2018") %>%
-  select(dna_id, dna_source, rbclx_accession, nostoc_phylogroup, reference) %>%
+  mutate(region = extract_before_comma_v(`voucher_published source`)) %>%
+  mutate(region = str_replace(region, ":", ",")) %>%
+  mutate(region = str_remove(region, "; Truong 3991")) %>%
+  select(dna_id, dna_source, rbclx_accession, nostoc_phylogroup, region, reference) %>%
   filter(rbclx_accession != "---")
 # Join all rbclx metadata tables
 public_rbclx_metadata <- bind_rows(obrien_2013, magain_2017, miadlikowska_2018, 
