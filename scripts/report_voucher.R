8ce28bce9be07b08592ca61c1643d7783f7527c9
cjpardodelahoz <cjpardodelahoz@gmail.com>
2023-12-11 21:33:44 -0500

    updated voucher table with Nics corrections, depths and plasmid lengths

--- scripts/report_voucher.R
+++ scripts/report_voucher.R
@@ -19,72 +19,61 @@ genome_ids_df <- genome_ids %>%
   left_join(plasmid_ids, by = "genome_id") %>%
   mutate(sample_id = 
            str_remove(genome_id, "_bin.*")) 
-# Load voucher primer table and merge with genome ids for set 103a
-voucher_df <- read_delim("misc_files/voucher_primer_3.txt", delim = "\t") %>%
-  left_join(genome_ids_df, by = "sample_id") %>%
-  relocate(sample_id, genome_id)
-write_csv(voucher_df, file = "document/tables/voucher_v1.csv")
-
-
-tree <- read.tree("analyses/phylogenetics/set103/divtime/1_part/mcmc/c1/dated.tree")
-tree_key <-  voucher_df %>%
-  filter(!is.na(genome_id)) %>%
-  mutate(tree_tip = 
-           paste(genome_id, ".fa", sep = "")) %>%
-  mutate(taxon_name = 
-           ifelse(genome_id == sample_id, 
-                  yes = taxon_name, 
-                  no = paste(genome_id, taxon_name, sep = "_"))) %>%
-  select(tree_tip, taxon_name)
-tree_new <- rename_taxa(tree, tree_key)
-write.tree(tree_new, file = "document/sharing/dated_tree.tree")  
-
-perrin_voucher <-  voucher_df %>%
-  filter(!is.na(genome_id)) %>%
-  mutate(tree_tip = 
-           paste(genome_id, ".fa", sep = "")) %>%
-  mutate(taxon_name = 
-           ifelse(genome_id == sample_id, 
-                  yes = taxon_name, 
-                  no = paste(genome_id, taxon_name, sep = "_"))) %>%
-  select(taxon_name, host_family, host_genus, substrate_type, lifestyle,
-         country,  locality, voucher, substrate_notes, reference)
-write_csv(perrin_voucher, 
-          file = "document/tables/nostoc_metadata_for_perrin_v1.csv")
-
-
-
-
-
-# Load checkm output table
-checkm_out <- read_delim("analyses/genome_qc/set8/checkm/checkm_out") %>%
-  mutate(`Bin Id` =
-           paste(`Bin Id`, ".fa", sep = ""))
-checkm_colnames <- stringr::str_replace(colnames(checkm_out), " ", "_") %>%
-  stringr::str_to_lower() 
-colnames(checkm_out) <- checkm_colnames
-# Load gunc output table
-gunc_out <- read_table("analyses/genome_qc/set8/gunc/GUNC.progenomes_2.1.maxCSS_level.tsv") %>%
-  mutate(genome =
-           paste(genome, ".fa", sep = ""))
 # Load and merge quast output
 quast_out <- merge_quast(sample_ids = genome_ids, 
-                         quast_out_dir = "analyses/genome_qc/set8/quast") %>%
+                         quast_out_dir = "analyses/genome_qc/set12c/quast") %>%
   mutate(Assembly = paste(Assembly, ".fa", sep = ""))
 # Load and merge busco output
 busco_out <- merge_busco(sample_ids = genome_ids, 
-                         busco_out_dir = "analyses/genome_qc/set8/busco/by_taxon/", 
+                         busco_out_dir = "analyses/genome_qc/set12c/busco/by_taxon/", 
                          busco_db = "run_nostocales_odb10", 
                          out_file_name = "full_table.tsv") 
 # Summarize busco output
 busco_sum <- busco_out %>% 
   sum_busco()
-# Join qc tables
-fullqc_set8 <- voucher_primer %>%
-  left_join(checkm_out, by = c("genome_id" = "bin_id")) %>%
-  left_join(gunc_out, by = c("genome_id" = "genome")) %>%
-  left_join(quast_out, by = c("genome_id" = "Assembly")) %>%
-  left_join(busco_sum, by = c("genome_id" = "sample_id"))
-# Make report directory and write qc table
-system("mkdir -p document/report_2")
-write_delim(fullqc_set8, file = "document/report_2/fullqc_set8.csv", delim = ",")
+# Load lifestyle metadata
+lifestyle_metadata <- read_csv("misc_files/set103_lifestyle_metadata.csv")
+# Load table with plasmid classification info and get plasmid lengths and chromosome depths
+depths_and_lenghts <- read.csv(file = "document/tables/contig_class_consensus.csv") %>%
+  mutate(genome_id = str_remove(genome_id, ".fa")) %>%
+  group_by(genome_id, contig_class) %>%
+  summarise(median_depth = median(totalAvgDepth), length = sum(contig_length)) %>%
+  ungroup(genome_id, contig_class) %>%
+  pivot_wider(names_from = contig_class, values_from = c(length, median_depth)) %>%
+  select(genome_id, median_depth_chromosome, length_plasmid) %>%
+  rename(chromosome_median_depth = median_depth_chromosome,
+         combined_plasmids_length = length_plasmid) %>%
+  mutate(chromosome_median_depth = case_when(
+    !is.na(chromosome_median_depth) ~ paste(round(chromosome_median_depth), "x", sep = ""),
+    !is.na(chromosome_median_depth) ~ NA))
+# Load voucher primer table and merge with genome ids for set 103a
+voucher_df <- read_delim("misc_files/voucher_primer_5.txt", delim = "\t") %>%
+  right_join(genome_ids_df, by = "sample_id") %>%
+  relocate(sample_id, genome_id) %>%
+  left_join(quast_out, by = c("chromosome_filename" = "Assembly")) %>%
+  left_join(busco_sum, by = c("chromosome_filename" = "sample_id")) %>%
+  left_join(lifestyle_metadata, by = c("genome_id" = "tip_label")) %>%
+  left_join(depths_and_lenghts, by = "genome_id") %>%
+  mutate(taxon_name = case_when(
+    str_detect(genome_id, taxon_name) | str_detect(taxon_name, genome_id) ~ taxon_name,
+    .default = paste(genome_id, taxon_name, sep = "_")),
+    percent_complete = round(percent_complete, digits = 1)) %>%
+  select(genome_id, taxon_name, voucher, site_id, lifestyle, lichen_substrate,
+         N50, "Total length", "GC (%)", chromosome_median_depth, percent_complete, combined_plasmids_length, accession, 
+         reference) %>%
+  rename("Taxon name" = taxon_name,
+         "Voucher" = voucher,
+         "Site ID" = site_id,
+         "Lifestyle"= lifestyle,
+         "Lichen substrate" = lichen_substrate,
+         "Chromosome N50" = N50,
+         "Chromosome total length (bp)" = "Total length",
+         "Chromosome median depth" = chromosome_median_depth,
+         "BUSCO completenes (%)" = percent_complete,
+         "Plasmids total length (bp)" = combined_plasmids_length,
+         "NCBI accession" = accession,
+         "Reference" = reference)
+# The genome labeled P6636 was mislabeled from the sequencing source and the actual code is P6236
+# This is to correct in the final table but all previous files will retain P6636
+voucher_df[107, 2] <- "P6236_bin_11_Peltigera_fuscopraetextata"
+write_csv(voucher_df, file = "document/tables/voucher_v2.csv")
